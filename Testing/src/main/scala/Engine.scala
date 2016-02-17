edcac053186e918e7e4e7de591550fc83d10e3d1
xuxiaomeng <xuxiaomeng@baozou.com>
2016-02-17 18:00:46 +0800

    feature. add probabilities.

--- Testing/src/main/scala/Engine.scala
+++ Testing/src/main/scala/Engine.scala
@@ -1,10 +1,12 @@
 package org.template.classification
 
+import org.apache.spark.mllib.linalg.DenseVector
+
 import io.prediction.controller.EngineFactory
 import io.prediction.controller.Engine
 
 class Query(
-  val features : Array[Double]
+  val features: Array[Double]
 ) extends Serializable {
   def getFeature(name: String) {
     features(name.toInt)
@@ -12,8 +14,12 @@ class Query(
 }
 
 class PredictedResult(
-  val label: Double
-) extends Serializable
+  val label: Double,
+  val query: Query,
+  val probabilities: DenseVector
+) extends Serializable {
+  def this(label: Double) = this(label, null, null)
+}
 
 class ActualResult(
   val label: Double
